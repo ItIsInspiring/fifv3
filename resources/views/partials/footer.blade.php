00752752e6ee22b8e6a6f2c612012ee59907fffc
vagrant <vagrant@homestead>
2021-04-14 18:23:23 +0000

    fixed and added fonts

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,21 @@
-<footer class="content-info">
+<footer class="content-info footer-fif">
+
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="row">
+      <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    </div>
   </div>
+  
+  <div class="container">
+    <div class="row">
+      {{ wp_nav_menu( array('menu'=>'social-links') ) }}
+    </div>
+  </div>
+  
+  <div class="container">
+    <div class="row">
+      {{ wp_nav_menu( array('menu'=>'footer-menu') ) }}
+    </div>
+  </div>
+
 </footer>
