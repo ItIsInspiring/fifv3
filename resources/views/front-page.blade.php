d8d9b50044002ce683e7b75b235e74f61b510fc5
faeriemoonlight <guillaumedeschamps75@gmail.com>
2021-04-19 22:43:03 +0200

    homepage finished

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -5,44 +5,44 @@
 <div class="intro">
     <img src="@asset('images/flash-info-fouffes-logo.png')" alt="">
     <h1>{{ get_bloginfo('name', 'display') }}</h1>
-    <p class="lead">{{ get_bloginfo('description', 'display') }}</p>
+    <p>{{ get_bloginfo('description', 'display') }}</p>
 </div>
-
+  
 <div class="cta ca-gratte">
-  <div class="row">
+  <div class="row bloc-content">
     <div class="col-md-6">
       <img src="@asset('images/flash-info-fouffes-logo.png')" alt="">
     </div>
     <div class="col-md-6">
-      <h2 class="display-4">Hello, world!</h2>
+      <h2>Ça me gratte !</h2>
       <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <div class="btn-fif"><a href="#" role="button">Learn more</a></div>
     </div>
 </div>
 </div>
 
 <div class="cta chat-perche">
-  <div class="row  flex-row-reverse">
+  <div class="row bloc-content flex-row-reverse">
     <div class="col-md-6">
       <img src="@asset('images/flash-info-fouffes-logo.png')" alt="">
     </div>
     <div class="col-md-6">
-      <h2 class="display-4">Hello, world!</h2>
+      <h2>Chat perché !</h2>
       <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <div class="btn-fif"><a href="#" role="button">Learn more</a></div>
     </div>
 </div>
 </div>
 
 <div class="cta soin-minou">
-  <div class="row">
+  <div class="row bloc-content">
     <div class="col-md-6">
       <img src="@asset('images/flash-info-fouffes-logo.png')" alt="">
     </div>
     <div class="col-md-6">
-      <h2 class="display-4">Hello, world!</h2>
+      <h2>Prends soin de ton minou !</h2>
       <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <div class="btn-fif"><a href="#" role="button">Learn more</a></div>
     </div>
 </div>
 </div>
