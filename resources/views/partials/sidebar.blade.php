173d5dc542f8cada8eb199c00e25477ce29c433a
zcqgjtk <zcqgjtk@ssh01.cluster028.gra.hosting.ovh.net>
2022-03-04 11:36:05 +0000

    add hook for widget

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid photocop photocop-rouge">
  <div class="row">
      <div class="col text-center">
-         <div class="px-4 py-5 mx-auto">
+         <div class="px-4 py-5<cod> mx-auto">
 
              @php dynamic_sidebar('sidebar-prefooter') @endphp
             </div>
